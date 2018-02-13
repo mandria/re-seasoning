079f23913c9a1af05f6ef42ed30fe2565648b4e8
Mandria <man@recipient.cc>
2018-02-13 23:07:49 +0100

    init

---
+++ re-seasoning.ino
@@ -0,0 +1,423 @@
+/*
+ _ _    _ _  _  _ _  _ . _  _
+| (/_  _\(/_(_|_\(_)| ||| |(_|
+                            _|
+Written by Mandria - recipient.cc
+Released under MIT licence.
+*/
+
+///////
+// mi mancano:
+//            -i timers
+//            -il get/put sulla eprom
+//            -le variazioni dei valori solo al movimento dei pot
+
+//potentiometers vars
+const int pot3Pin = A0;
+const int pot2Pin = A2;
+const int pot1Pin = A3;
+int pot1Val = 0;
+int pot2Val = 0;
+int pot3Val = 0;
+int pot1ValOld = 0;
+int pot2ValOld = 0;
+int pot3ValOld = 0;
+//button vars
+#include "OneButton.h"
+const int buttPin = 12;
+OneButton button(buttPin, true);
+// menu
+//LV0 reading | LV1 temp menu | LV2 hum menu | LV3 repeat and duration | LV4 on off switch functions | LV5 writing parameters to eprom and get back to level 0
+int menuLevel = 0;
+const int maxLevel = 5;
+// relay pins R1-P7 | R2-P6 | R3-P5 | R4-P4
+int Fridge = 7;
+int Humidifier = 6;
+int extFan = 5;
+int movFan = 4;
+//temerature and humidity
+float ft;
+float fh;
+int t;
+int h;
+//Treshold and Timer settings
+//T and H Threshold
+int thresT; // =0?; 2° tollerance suggested
+int thresH; // =0?; 3% tollerance suggested
+int tollT; //tolerrance temperature
+int tollH; //tollerance humidity
+//
+bool hum;
+bool extr;
+bool mfan;
+
+//Libraries and imports
+//LCD
+#include <Wire.h>
+#include <LCD03.h>
+LCD03 lcd;
+//DHT
+#include "DHT.h"
+#define DHTPIN 2
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+//EPROM
+#include <EEPROM.h>
+int eeAddress = 0;
+//Timers
+#include <elapsedMillis.h>
+//timers
+elapsedMillis timerFan;
+elapsedMillis timerMovFanDur;
+elapsedMillis timerExtFanDur;
+elapsedMillis timerSensRead;
+elapsedMillis timerBackLit;
+//timer interval internal air (in mS)
+int MovFanTimer;
+int MovFanDur;
+int ExtFanDur;
+int BackLitDur = 5000;
+// verificare come tenere accesa la ventola per tot secondi dopo che il timer ha raggiunto il suo valore
+
+
+void setup() {
+Serial.begin(9600);
+//pinMode(buttPin, INPUT_PULLUP);
+pinMode(Fridge, OUTPUT);
+pinMode(Humidifier, OUTPUT);
+pinMode(extFan, OUTPUT);
+pinMode(movFan, OUTPUT);
+dht.begin();  
+button.attachClick(click1);
+button.attachLongPressStart(longPressStart1);
+button.attachLongPressStop(longPressStop1);
+button.attachDuringLongPress(longPress1);
+lcd.begin(16, 2);
+lcd.backlight();
+lcd.print("Loading         Configuration...");
+delay(2000);
+lcd.clear();
+}
+
+void loop() {
+  button.tick();
+  readPots();
+  constMenu();
+
+//  if (timer0 > interval) {
+//    timer0 -= interval; //reset the timer
+//    // read the current state and write the opposite
+//    digitalWrite(led, !ledPin);
+//  }
+
+}
+
+//function to display information on menu tabs
+void constMenu(){
+  /////////////////////visualization data
+    if(menuLevel==0){
+    lcd.home();
+    lcd.print(" TMP  HUM  TIMER");
+    if (t < 10){
+      lcd.print("0");
+      lcd.print(t);
+    } else{
+      lcd.print(t);
+    }
+    lcd.print((char)223);
+    lcd.print("C  ");
+    if (h < 10){
+      lcd.print("0");
+      lcd.print(h);
+    } else{
+      lcd.print(h);
+    }
+    lcd.print("% ");
+    int seconds=timerMovFanDur%60000/1000;
+    int minutes=timerMovFanDur/1000/60;
+    if (minutes < 10){
+      lcd.print("00");
+      lcd.print(minutes);
+    } else if(minutes < 100){
+      lcd.print("0");
+      lcd.print(minutes);
+    }else{
+      lcd.print(minutes);
+    }
+    lcd.print(".");
+    if (seconds < 10){
+      lcd.print("0");
+      lcd.print(seconds);
+    } else {
+      lcd.print(seconds);
+    }
+    //lcd.print(timerMovFanDur);
+  }
+  ///////////////////// TEMPERATURE
+  if(menuLevel==1){
+    lcd.home();
+    lcd.print("TRHT TTOL       ");
+    if (pot1Val < 10){
+      lcd.print("0");
+      lcd.print(pot1Val);
+    } else{
+      lcd.print(pot1Val);
+    }
+    lcd.print((char)223);
+    lcd.print("C ");
+    if (pot2Val < 10){
+      lcd.print("0");
+      lcd.print(pot2Val);
+    } else{
+      lcd.print(pot2Val);
+    }
+    lcd.print((char)223);
+    lcd.print("C  ");
+    //set var from pot
+    thresT=pot1Val;
+    tollT=pot2Val;
+  }
+  /////////////////////// HUMIDITY
+  if(menuLevel==2){
+    lcd.home();
+    lcd.print("TRHH HTOL       ");
+    lcd.print(" ");
+    if (pot1Val < 10){
+      lcd.print("0");
+      lcd.print(pot1Val);
+    } else{
+      lcd.print(pot1Val);
+    }
+    lcd.print("% ");
+    if (pot2Val < 10){
+      lcd.print("0");
+      lcd.print(pot2Val);
+    } else{
+      lcd.print(pot2Val);
+    }
+    lcd.print("%  ");
+    //set var from pot
+    thresH=pot1Val;
+    tollH=pot2Val;
+  }
+  /////////////////////// set repeating timers and duration
+  if(menuLevel==3){
+    lcd.home();
+    lcd.print("MVTI MVDR EXTFDR");
+    lcd.print("m");
+    if (pot1Val < 10){
+      lcd.print("00");
+      lcd.print(pot1Val);
+    }else if(pot1Val < 100){
+      lcd.print("0");
+      lcd.print(pot1Val);
+    } else{
+      lcd.print(pot1Val);
+    }
+    lcd.print("  s");
+    if (pot2Val < 10){
+      lcd.print("00");
+      lcd.print(pot2Val);
+    } else if(pot2Val < 100){
+      lcd.print("0");
+      lcd.print(pot2Val);
+    } else{
+      lcd.print(pot2Val);
+    }
+    lcd.print("  s");
+    if (pot3Val < 10){
+      lcd.print("00");
+      lcd.print(pot3Val);
+    }else if(pot3Val < 100){
+      lcd.print("0");
+      lcd.print(pot3Val);
+    } else{
+      lcd.print(pot3Val);
+    }
+    //set var from pot
+    MovFanTimer=pot1Val;
+    MovFanDur=pot2Val;
+    ExtFanDur=pot3Val;
+  }
+  ///////////////////////
+  if(menuLevel==4){
+    lcd.home();
+    lcd.print(" HUM  EXTF  MFAN");
+    if(pot1Val==0){
+      lcd.print(" off"); 
+    } else {
+      lcd.print("  on"); 
+    }
+    if(pot2Val==0){
+      lcd.print("   off"); 
+    } else {
+      lcd.print("    on"); 
+    }
+    if(pot3Val==0){
+      lcd.print("   off"); 
+    } else {
+      lcd.print("    on"); 
+    }
+   hum=pot1Val;
+   extr=pot2Val;
+   mfan=pot3Val;
+    
+  }
+  ///////////////////////
+  if(menuLevel==5){
+    //lcd.home();
+    //lcd.print("writing to eprom");
+  }
+}
+//function to trigger one time action from menu changes
+void oneTimeMenu(){
+  if(menuLevel==0){
+    lcd.clear();
+    //lcd.home();
+    //lcd.print("Level0 reding sensor");
+  }
+  if(menuLevel==1){
+    lcd.clear();
+    //lcd.home();
+    //lcd.print("setting temp");
+  }
+  if(menuLevel==2){
+    lcd.clear();
+    //lcd.home();
+    //lcd.print("setting hum");
+  }
+  if(menuLevel==3){
+    lcd.clear();
+    //lcd.home();
+    //lcd.print("setting repetition");
+  }
+  if(menuLevel==4){
+    lcd.clear();
+    //lcd.home();
+    //lcd.print("switch on off");
+  }
+  if(menuLevel==5){
+    lcd.clear();
+    lcd.home();
+    lcd.print("writing settings");
+  }
+}
+
+void readPots(){
+
+
+if (menuLevel >= 1 && menuLevel <= 4){
+if(abs(pot1Val-pot1ValOld)>2){
+  pot1Valold=pot1Val;
+  Serial.print("Value changed");
+}
+pot1Val = analogRead(pot1Pin);
+pot2Val = analogRead(pot2Pin);
+pot3Val = analogRead(pot3Pin);
+//debug
+//Serial.println(menuLevel);
+//Serial.println("reading pots");
+if(menuLevel==1){
+  //temperature reading map
+  //temp
+  pot1Val = map (pot1Val, 0, 1023, 1, 30);
+  //temp toll
+  pot2Val = map (pot2Val, 0, 1023, 1, 10);
+  //Serial.println(pot1Val);
+  //Serial.println(pot2Val);
+}
+if(menuLevel==2){
+  //Humidity reading map
+  //hum
+  pot1Val = map (pot1Val, 0, 1023, 0, 99);
+  //hum toll
+  pot2Val = map (pot2Val, 0, 1023, 1, 10);
+  
+}
+if(menuLevel==3){
+  //repeat and duration
+  //mov fan repeat timer from 1 to 30 min (x * 60 *1000 = x *60000)
+  pot1Val = map (pot1Val, 0, 1023, 1, 60);
+  //mov fan duration in seconds
+  pot2Val = map (pot2Val, 0, 1023, 5, 120);
+  //ext fan duration in seconds
+  pot3Val = map (pot3Val, 0, 1023, 5, 120);
+  
+}
+if(menuLevel==4){
+ //I/O outputs
+  //HUM IO
+  pot1Val = map (pot1Val, 0, 512, 0, 1);
+  //ETRACT FAN
+  pot2Val = map (pot2Val, 0, 512, 0, 1);
+  //MOV FAN
+  pot3Val = map (pot3Val, 0, 512, 0, 1);
+   
+}
+}
+}
+
+
+void click1() {    
+  if(menuLevel <= 3){
+  menuLevel++;
+  } else{
+  menuLevel=0;  
+  }
+  oneTimeMenu();
+} // click1
+// This function will be called once, when the button1 is pressed for a long time.
+void longPressStart1() {
+  menuLevel=5;
+  oneTimeMenu();
+} // longPressStart1
+
+// This function will be called often, while the button1 is pressed for a long time.
+void longPress1() {
+  Serial.println("Button 1 longPress...");
+} // longPress1
+
+// This function will be called once, when the button1 is released after beeing pressed for a long time.
+void longPressStop1() {
+  Serial.println("Button 1 longPress stop");
+} // longPressStop1
+
+
+
+void FunctionalLoop(){
+  int HTU = thresH+tollH;
+  int HTD = thresH-tollH;
+  int TT = thresT+tollT;
+  fh = dht.readHumidity();
+  ft = dht.readTemperature();
+  h=(int)fh;
+  t=(int)ft;
+ if (isnan(h) || isnan(t)) {
+    lcd.home(); 
+    lcd.print("Failed to read from DHT sensor!");
+    return;
+  }
+  
+ if (t > TT){
+  digitalWrite(Fridge, HIGH);
+ } else{
+  digitalWrite(Fridge, LOW);
+ }
+ if (h > HTU){
+  digitalWrite(extFan, HIGH);
+ } else {
+  digitalWrite(extFan, LOW);
+ }
+ if (h < HTD ){
+  digitalWrite(Humidifier, HIGH);
+ } else {
+  digitalWrite(Humidifier, LOW); 
+ }
+
+}
+
+void Timers(){
+  
+
+}
+
